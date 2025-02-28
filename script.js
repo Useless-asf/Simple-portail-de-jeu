325f46f03f54fcad85f46db647fc4d76ce52df27
Tao <taodufourbez@gmail.com>
2025-02-28 17:45:12 +0100

    Update 0.2

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-gamesCards = document.querySelectorAll(".game-card");
 
-gamesCards.forEach(i => {
-    i.addEventListener('click', (e) => {
-        game = e.currentTarget.dataset.game;
-        console.log(game);
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    //      Ajout des events de clicks sur les game-card pour changer quel est la page actuel
+    
+    const gamesCards = document.querySelectorAll(".game-card");
+    const gameSections = document.querySelectorAll(".game-section");
+    const menu = document.getElementById("menu");
+    
+    gamesCards.forEach(card => {
+        card.addEventListener("click", (e) => {
+            const game = e.currentTarget.dataset.game;
+
+            menu.style.display = "none";
+
+            gameSections.forEach(section => {
+                section.style.display = "none";
+            });
+
+            const selectedGame = document.getElementById(game);
+            if (selectedGame) {
+                selectedGame.style.display = "flex";
+            }
+        })
     })
-});
+
+    const returnButtons = document.querySelectorAll(".return-button");
+
+    returnButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            gameSections.forEach(section => {
+                section.style.display = "none";
+            })
+
+            menu.style.display = "flex";
+        })
+    })
+
+
+})
